edd77a01ec366fca48c2b773e6e28d33d78cad64
jesusgr <jesusgr9511@gmail.com>
2025-05-08 10:28:26 -0600

    Se agregan archivos al proyecto

---
+++ Cuadrado.java
@@ -0,0 +1,43 @@
+
+public class Cuadrado {
+	private String nombre;
+	private double lado;
+	
+	public Cuadrado(String nombre, double lado) {
+		super();
+		this.nombre = nombre;
+		this.lado = lado;
+	}//constructor
+	
+	public double calcularArea() {
+		return (getLado() * getLado());
+	}
+	
+	public double calcularPerimetro() {
+		return 4*(getLado());
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getLado() {
+		return lado;
+	}
+
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+
+	@Override
+	public String toString() {
+		return "Cuadrado [nombre=" + nombre + ", lado=" + lado + "]";
+	}
+	
+	
+
+}//class
